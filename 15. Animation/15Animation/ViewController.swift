fd5a90797c5a416d38d0531fc6574641bd0c32c3
sblsatwikha <43727134+sblsatwikha@users.noreply.github.com>
2024-04-27 00:53:28 -0400

    Project 15: Animation
    - Bringing interfaces to life with animation.
    - Core Animation, CGAffineTransform

---
+++ 15. Animation/15Animation/ViewController.swift
@@ -0,0 +1,63 @@
+//
+//  ViewController.swift
+//  15Animation
+//
+//  Created by Satwika on 4/21/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var imageView: UIImageView!
+    var currentAnimation = 0
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        imageView = UIImageView(image: UIImage(named: "penguin"))
+        imageView.center = CGPoint(x: 512, y: 384)
+        view.addSubview(imageView)
+    }
+
+    @IBAction func tapped(_ sender: UIButton) {
+        
+        sender.isHidden = true
+        
+//        UIView.animate(withDuration: 1, delay: 0, animations: {
+        UIView.animate(withDuration: 1, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 5, animations: {
+            switch self.currentAnimation {
+            case 0:
+                self.imageView.transform = CGAffineTransform(scaleX: 2, y: 2)
+            case 1:
+                self.imageView.transform = .identity
+            case 2:
+                self.imageView.transform = CGAffineTransform(translationX: -256, y: -256)
+            case 3:
+                self.imageView.transform = .identity
+            case 4:
+                self.imageView.transform = CGAffineTransform(rotationAngle: .pi)
+            case 5:
+                self.imageView.transform = .identity
+            case 6:
+                self.imageView.alpha = 0.1
+            case 7:
+                self.imageView.alpha = 1
+            default:
+                print("deffff")
+                
+            }
+        }) { finished in
+            sender.isHidden = false
+        }
+        
+        currentAnimation += 1
+        
+        if currentAnimation>7 {
+            currentAnimation = 0
+        }
+    }
+    
+   
+}
+
