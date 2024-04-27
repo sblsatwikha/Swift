685aef943420b00b185e76861a460839c23d5f71
sblsatwikha <43727134+sblsatwikha@users.noreply.github.com>
2024-04-27 00:45:07 -0400

    Project 4: Easy Browser
    - Embed Web Kit and learnt about delegation, KVO, classes and UIToolbar.
    - loadView(), WKWebView, URLRequest, UIToolbar, UIProgressView, key-value observing

---
+++ 4. EasyBrowser/EasyBrowser/ViewController.swift
@@ -0,0 +1,81 @@
+//
+//  ViewController.swift
+//  EasyBrowser
+//
+//  Created by Satwika on 4/3/24.
+//
+
+import UIKit
+import WebKit
+
+class ViewController: UIViewController, WKNavigationDelegate {
+    
+    var webView: WKWebView!
+    var progressView: UIProgressView!
+    var webSites = ["apple.com", "hackingwithswift.com"]
+    
+    override func loadView() {
+        webView = WKWebView()
+        webView.navigationDelegate = self
+        view = webView
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Open", style: .plain, target: self, action: #selector(openTapped))
+        
+        let flexibleSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        let refresh = UIBarButtonItem(barButtonSystemItem: .refresh, target: webView, action: #selector(webView.reload))
+        let webBack = UIBarButtonItem(title: "Back", image: nil, target: webView, action: #selector(webView.goBack))
+        
+        let webFront = UIBarButtonItem(title: "Front", style: .plain, target: webView, action: #selector(webView.goForward))
+        
+        progressView = UIProgressView(progressViewStyle: .default)
+        progressView.sizeToFit()
+    
+        
+        let progressiveBarButton = UIBarButtonItem(customView: progressView)
+        
+        
+        toolbarItems = [webBack, progressiveBarButton, flexibleSpace, refresh, webFront]
+        navigationController?.isToolbarHidden = false
+        
+        // Do any additional setup after loading the view.
+        let url = URL(string: webSites[0])!
+        webView.load(URLRequest(url: url))
+        webView.allowsBackForwardNavigationGestures = true
+        
+        
+        webView.addObserver(self, forKeyPath: #keyPath(WKWebView.estimatedProgress), context: nil)
+    }
+    
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+        if keyPath == "estimatedProgress" {
+            progressView.progress = Float(webView.estimatedProgress)
+        }
+    }
+    
+    @objc func openTapped() {
+        let ac = UIAlertController(title: "Open page…", message: nil, preferredStyle: .actionSheet)
+        
+        for webSite in webSites {
+            ac.addAction(UIAlertAction(title: webSite, style: .default, handler: openPage))
+        }
+
+        ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        ac.popoverPresentationController?.barButtonItem = self.navigationItem.rightBarButtonItem
+        present(ac, animated: true)
+    }
+    
+    func openPage(action: UIAlertAction) {
+        let url = URL(string: "https://" + action.title!)!
+        webView.load(URLRequest(url: url))
+    }
+    
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+//        title = webView.title
+    }
+
+
+}
+
