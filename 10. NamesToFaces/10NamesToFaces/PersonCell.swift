d6fe747f3fa4a3d42db7d29444aaa709c171d094
sblsatwikha <43727134+sblsatwikha@users.noreply.github.com>
2024-04-27 00:49:44 -0400

    Project 10: Names to Faces
    - Worked on UICollectionView and the photo library.
    - UICollectionView, UIImagePickerController, UUID, classes

---
+++ 10. NamesToFaces/10NamesToFaces/PersonCell.swift
@@ -0,0 +1,14 @@
+//
+//  PersonCell.swift
+//  10NamesToFaces
+//
+//  Created by Satwika on 4/11/24.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    
+    @IBOutlet var name: UILabel!
+    @IBOutlet var imageView: UIImageView!
+}
